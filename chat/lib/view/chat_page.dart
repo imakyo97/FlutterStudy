42b663d357fbbc23019a8d91067a1325d6e1b30d
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-04-06 23:17:41 +0900

    入力した内容をFirestoreに保存するよう実装

--- chat/lib/view/chat_page.dart
+++ chat/lib/view/chat_page.dart
@@ -1,3 +1,7 @@
+import 'package:chat/main.dart';
+import 'package:chat/model/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -15,7 +19,28 @@ class _ChatPageState extends State<ChatPage> {
         title: const Text('チャット'),
       ),
       body: Center(
-        child: TextFormField(),
+        child: TextFormField(
+          onFieldSubmitted: ((text) {
+            final user = FirebaseAuth.instance.currentUser!;
+
+            final posterId = user.uid;
+            final posterName = user.displayName!;
+            final posterImageUrl = user.photoURL!;
+
+            final newDocumentReference = postsReference.doc();
+
+            final newPost = Post(
+              text: text,
+              createdAt: Timestamp.now(), // 投稿日時は現在とします
+              posterName: posterName,
+              posterImageUrl: posterImageUrl,
+              posterId: posterId,
+              reference: newDocumentReference,
+            );
+
+            newDocumentReference.set(newPost);
+          }),
+        ),
       ),
     );
   }
