d0268d895cb3555c0c1fe13375a8a6fdb8ed8809
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-04-09 23:43:50 +0900

    リファクタリングと細かい機能追加

--- chat/lib/view/chat_page.dart
+++ chat/lib/view/chat_page.dart
@@ -14,74 +14,111 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    textEditingController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const MyPage();
-                  },
-                ),
-              );
-            },
-            child: CircleAvatar(
-              backgroundImage:
-                  NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!),
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot<Post>>(
-              stream: postsReference.orderBy('createdAt').snapshots(),
-              builder: (context, snapshot) {
-                final docs = snapshot.data?.docs ?? [];
-                return ListView.builder(
-                  itemCount: docs.length,
-                  itemBuilder: (context, index) {
-                    final post = docs[index].data();
-                    return PostWidget(post: post);
-                  },
+    return GestureDetector(
+      onTap: () {
+        primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('チャット'),
+          actions: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const MyPage();
+                    },
+                  ),
                 );
               },
+              child: CircleAvatar(
+                backgroundImage:
+                    NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot<Post>>(
+                stream: postsReference.orderBy('createdAt').snapshots(),
+                builder: (context, snapshot) {
+                  final docs = snapshot.data?.docs ?? [];
+                  return ListView.builder(
+                    itemCount: docs.length,
+                    itemBuilder: (context, index) {
+                      final post = docs[index].data();
+                      return PostWidget(post: post);
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-          Center(
-            child: TextFormField(
-              onFieldSubmitted: ((text) {
-                final user = FirebaseAuth.instance.currentUser!;
-
-                final posterId = user.uid;
-                final posterName = user.displayName!;
-                final posterImageUrl = user.photoURL!;
-
-                final newDocumentReference = postsReference.doc();
-
-                final newPost = Post(
-                  text: text,
-                  createdAt: Timestamp.now(), // 投稿日時は現在とします
-                  posterName: posterName,
-                  posterImageUrl: posterImageUrl,
-                  posterId: posterId,
-                  reference: newDocumentReference,
-                );
-
-                newDocumentReference.set(newPost);
-              }),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextFormField(
+                  controller: textEditingController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(color: Colors.amber),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(
+                        color: Colors.amber,
+                        width: 2,
+                      ),
+                    ),
+                    fillColor: Colors.amber[50],
+                    filled: true,
+                  ),
+                  onFieldSubmitted: (text) {
+                    sendPost(text);
+                    textEditingController.clear();
+                  },
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> sendPost(String text) async {
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final posterId = user.uid;
+    final posterName = user.displayName!;
+    final posterImageUrl = user.photoURL!;
+
+    final newDocumentReference = postsReference.doc();
+
+    final newPost = Post(
+      text: text,
+      createdAt: Timestamp.now(), // 投稿日時は現在とします
+      posterName: posterName,
+      posterImageUrl: posterImageUrl,
+      posterId: posterId,
+      reference: newDocumentReference,
+    );
+
+    newDocumentReference.set(newPost);
   }
 }
 
@@ -140,12 +177,37 @@ class PostWidget extends StatelessWidget {
                       child: Text(post.text),
                     ),
                     if (FirebaseAuth.instance.currentUser!.uid == post.posterId)
-                      IconButton(
-                        onPressed: () {
-                          // 削除は reference に対して delete() を呼ぶだけでよい。
-                          post.reference.delete();
-                        },
-                        icon: const Icon(Icons.delete),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    content: TextFormField(
+                                      initialValue: post.text,
+                                      autofocus: true,
+                                      onFieldSubmitted: (newText) {
+                                        post.reference
+                                            .update({'text': newText});
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            icon: const Icon(Icons.edit),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              // 削除は reference に対して delete() を呼ぶだけでよい。
+                              post.reference.delete();
+                            },
+                            icon: const Icon(Icons.delete),
+                          ),
+                        ],
                       ),
                   ],
                 ),
