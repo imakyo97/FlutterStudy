05875b797bb53f492af637d2273c6caf95c113b2
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-10-15 20:01:11 +0900

    メソッドチャネルでネイティブコードを呼び出す処理を実装

--- for_ios_method_channel/ios/Runner/AppDelegate.swift
+++ for_ios_method_channel/ios/Runner/AppDelegate.swift
@@ -3,11 +3,29 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // rootViewControllerから表示の元となるFlutterViewControllerを取り出す
+        let controller = window.rootViewController as! FlutterViewController
+        // メソッドチャネルに任意の名前を付け、controllerのbinaryMessengerを使って初期化
+        let methodChannel = FlutterMethodChannel(name: "for_ios_method_channel/for_study", binaryMessenger: controller.binaryMessenger)
+
+        // flutter側でgoodbyeWorldが呼ばれたらcallGoodbyeWorldを呼び出す
+        methodChannel.setMethodCallHandler { [weak self] methodCall, result in
+            guard methodCall.method == "goodbyeWorld", let strongSelf = self else {
+                return result(FlutterMethodNotImplemented)
+            }
+            return result(strongSelf.callGoodbyeWorld())
+        }
+
+        // 以下は既存コード
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    private func callGoodbyeWorld() -> String {
+        return "Goodbye World!"
+    }
 }
--- for_ios_method_channel/lib/main.dart
+++ for_ios_method_channel/lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String centerMessage = 'Hello World!';
+  // AppDelegateで設定した名前と一致するmethodChannelを作成
+  static const methodChannel =
+      MethodChannel('for_ios_method_channel/for_study');
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(centerMessage),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.self_improvement_sharp),
+          onPressed: () => callGoodByeWorld(),
         ),
       ),
     );
+  }
+
+  // methodChannelで関数を呼び出し、結果を画面に表示
+  Future<void> callGoodByeWorld() async {
+    String result = '';
+    try {
+      result = await methodChannel.invokeMethod('goodbyeWorld');
+    } on PlatformException catch (e) {
+      result = e.message ?? 'error';
+    }
+
+    setState(() {
+      centerMessage = result;
+    });
   }
 }
