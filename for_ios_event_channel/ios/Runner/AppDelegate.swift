73379ed0c246d22c1070d4702b96ed990ebbb429
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-10-22 00:11:47 +0900

    イベントチャネルで10秒カウントするメソッドを実装

--- for_ios_event_channel/ios/Runner/AppDelegate.swift
+++ for_ios_event_channel/ios/Runner/AppDelegate.swift
@@ -3,11 +3,45 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // rootViewControllerから表示の元となるFlutterViewControllerを取り出す
+        let controller = window.rootViewController as! FlutterViewController
+        // イベントチャネルに任意の名前を付け、controllerのbinaryMessengerを使って初期化
+        let eventChannel = FlutterEventChannel(name: "for_ios_event_channel/for_study", binaryMessenger: controller.binaryMessenger)
+
+        // flutter側でcountTenが呼ばれたらcountTenを呼び出す
+        let countTenStreamHandler = CountTenStreamHandler()
+        eventChannel.setStreamHandler(countTenStreamHandler)
+
+        // 以下は既存コード
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
+
+class CountTenStreamHandler: NSObject, FlutterStreamHandler {
+    // 非同期にイベントを発行するコールバック。
+    // FlutterErrorでコールバックを呼び出すと、エラーイベントが発生します。
+    // FlutterEndOfEventStreamを指定してコールバックを呼び出すと、これ以上イベントが発行されないことを示します。
+    // nilを含むその他の値は成功イベントとして放出されます。
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        var count = 0
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            count += 1
+            events(String(count))
+            if count >= 10 {
+                timer.invalidate()
+                events(FlutterEndOfEventStream)
+            }
+        })
+        return nil
+    }
+
+    // Flutter側でこのチャンネルに関連付けられたStreamから最後のリスナーが登録解除されたときに呼び出される。
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        return nil
+    }
 }
--- for_ios_event_channel/lib/main.dart
+++ for_ios_event_channel/lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String count = "0";
+  // AppDelegateで設定した名前と一致するイベントチャネルを作成
+  static const eventChannel = EventChannel('for_ios_event_channel/for_study');
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            count,
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.self_improvement_sharp),
+          onPressed: () => countTen(),
         ),
       ),
     );
+  }
+
+  void countTen() {
+    eventChannel.receiveBroadcastStream().listen((event) {
+      if (event is String) {
+        setState(() {
+          count = event;
+        });
+      }
+    });
   }
 }
